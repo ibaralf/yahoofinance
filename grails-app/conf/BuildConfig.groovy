918e90eeaadec858f88c220a204efd8dd30215d0
Burt Beckwith <burt@burtbeckwith.com>
2013-03-07 00:34:07 -0500

    updated to the latest release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,4 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -30,9 +28,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:2.0.3",
-              ":rest-client-builder:1.0.2") {
+        build(":release:2.2.1", ":rest-client-builder:1.0.3") {
             export = false
         }
     }
