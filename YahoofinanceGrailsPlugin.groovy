ec5e17609baea67a7e3539c9c4d0e5fd2b9ea028
ibaralf <ibarra.alfonso@gmail.com>
2013-03-07 11:48:47 -0800

    Update YahoofinanceGrailsPlugin.groovy
    Added SCM and issueManagement info.

--- YahoofinanceGrailsPlugin.groovy
+++ YahoofinanceGrailsPlugin.groovy
@@ -41,10 +41,9 @@ JSON to object.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/ibaralf/yahoofinance/issues" ]
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/ibaralf/yahoofinance" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
