bc4236614ce09a91b59ab7c1823064f577d4d98a
Burt Beckwith <burt@burtbeckwith.com>
2013-03-07 01:47:34 -0500

    don't use jar files in the lib directory - use dependencies in BuildConfig.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,6 +12,12 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
+		compile 'org.codehaus.jackson:jackson-core-asl:1.9.9'
+		compile 'org.codehaus.jackson:jackson-mapper-asl:1.9.9'
+		compile 'net.sf.opencsv:opencsv:2.3'
+		compile 'commons-httpclient:commons-httpclient:3.1', {
+			excludes 'commons-codec', 'commons-logging', 'junit'
+		}
 	}
 
 	plugins {
