af05898448a99d9e16570f2bf86f313a82b8280b
Burt Beckwith <burt@burtbeckwith.com>
2013-03-07 01:11:34 -0500

    never instantiate Spring beans - use dependency injection

--- grails-app/taglib/com/ibaralf/StockQuoteTagLib.groovy
+++ grails-app/taglib/com/ibaralf/StockQuoteTagLib.groovy
@@ -5,7 +5,8 @@ import com.ibaralf.YahooFinanceYQLService
 class StockQuoteTagLib {
 
 	static namespace = 'yahoofinance'
-	def yahooFinanceYQLService = new YahooFinanceYQLService()
+
+	def yahooFinanceYQLService
 		
 	def quote = { attrs, body ->
 		println "\n\nINTAGLIB ${attrs.symbol} && ${attrs.data}"
